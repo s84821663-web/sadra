899400c82df4083cf5d78d79b1f499fecda74487
s84821663-web <s84821663@gmail.com>
2026-07-24 18:38:02 +0330

    Update script.js

--- script.js
+++ script.js
@@ -399,3 +399,21 @@ img.style.transform =
 console.log(
 "Urban Taste | Designed by You ❤️"
 );
+// کدهای قبلی سایت
+...
+
+// کدهای دارک مود
+...
+
+// ===== سبد خرید =====
+
+let cart = [];
+
+function addToCart(name, price) {
+    cart.push({
+        name: name,
+        price: price
+    });
+
+    alert(name + " به سبد خرید اضافه شد.");
+}
